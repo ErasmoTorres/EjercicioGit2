8f150212fc7d14ba30de147320f07dd4542553e7
erasm <erasm@WT>
2024-03-06 19:10:00 +0100

    Lineas del metodo main

---
+++ src/javaapplication5/Principal.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication5;
+
+/**
+ *
+ * @author erasm
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("linea 1 del metodo main");
+        System.out.println("linea 2 del metodo main");
+        // TODO code application logic here
+        /*https://github.com/ErasmoTorres/EjercicioGit2.git
+        tok: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*/
+        
+    }
+    
+}
