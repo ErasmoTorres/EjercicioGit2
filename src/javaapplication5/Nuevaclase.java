a1adcf7426dd6a86babb602183d84ee136b847f9
erasm <erasm@WT>
2024-03-06 19:15:19 +0100

    Se ha creado la rama ClaseNueva

---
+++ src/javaapplication5/Nuevaclase.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaapplication5;
+
+/**
+ *
+ * @author erasm
+ */
+public class Nuevaclase {
+   public static void main ( String[] args){
+       
+    System.out.println("En la rama se crea la clase nueva.");
+   }
+}
